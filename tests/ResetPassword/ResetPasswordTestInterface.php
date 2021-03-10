a93199aff5fd0c85fb184be20957975114a29e09
Mathieu Margotin <mathieu.margotin@gmail.com>
2021-03-10 16:01:09 +0100

    update ResetPasswordTestInterface

--- tests/ResetPassword/ResetPasswordTestInterface.php
+++ tests/ResetPassword/ResetPasswordTestInterface.php
@@ -8,4 +8,5 @@ interface ResetPasswordTestInterface
 {
     public function testResetPasswordWithValidToken(): void;
     public function testResetPasswordWithInValidToken(): void;
+    public function testResetPasswordWithExpiredToken(): void;
 }
